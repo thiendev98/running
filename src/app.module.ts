cbaebc1a862c5abb9638c32e9ed54c4d552f9642
hoangthien <thien.dohoangcs98@gmail.com>
2023-04-03 22:51:22 +0700

    update web services with nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { RunsModule } from './runs/runs.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+@Module({
+  imports: [RunsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
